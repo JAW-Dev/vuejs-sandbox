fc1778d32d1f3a5896816b198c85c4cb75a526d4
Jason Witt <jawittdesigns@gmail.com>
2018-07-23 11:25:10 -0400

    Updated base URL

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ const baseURL = (process.env.NODE_ENV === 'production') ? 'https://jawittdesigns
 
 export default new Router({
 	mode: 'history',
-	base: baseURL,
+	base: '/',
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/autocomplete-field', name: 'Autocomplete Field', component: AutocompleteField },
