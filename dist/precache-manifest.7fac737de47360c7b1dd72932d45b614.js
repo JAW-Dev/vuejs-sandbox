37aa8636c95e61964c1a3cf0436a6460490c65ca
Jason Witt <jawittdesigns@gmail.com>
2018-07-23 11:00:52 -0400

    Added dist dir

---
+++ dist/precache-manifest.7fac737de47360c7b1dd72932d45b614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://jawittdesigns.github.io/vuejs-sandbox/dist/robots.txt"
+  },
+  {
+    "revision": "7275672b2a4dd1907cf7",
+    "url": "https://jawittdesigns.github.io/vuejs-sandbox/dist/js/chunk-vendors.7275672b.js"
+  },
+  {
+    "revision": "2bc91261550b4d9b2120",
+    "url": "https://jawittdesigns.github.io/vuejs-sandbox/dist/js/app.2bc91261.js"
+  },
+  {
+    "revision": "581b033b655a8c1b5cb1033ec7ed6f13",
+    "url": "https://jawittdesigns.github.io/vuejs-sandbox/dist/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "https://jawittdesigns.github.io/vuejs-sandbox/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2bc91261550b4d9b2120",
+    "url": "https://jawittdesigns.github.io/vuejs-sandbox/dist/css/app.a460ddec.css"
+  }
+];
