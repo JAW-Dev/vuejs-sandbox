fa26d5b5cb5b4dfb487a8321f295568c4be802da
Jason Witt <jawittdesigns@gmail.com>
2018-07-26 16:29:16 -0400

    Simplified article component

--- src/components/ArticleCard/ArticleCards.vue
+++ src/components/ArticleCard/ArticleCards.vue
@@ -1,25 +1,23 @@
 <template>
 	<section class="articles" >
-		<ArticleCard 
-			v-if="responseData" 
-			v-for="(data, index) in responseData"
-			:key="index"
-			:url="data.url"
-			:image="data.image"
-			:description="data.description"
-			:author="data.author"
-			:authorLabel="data.authorLabel"
-			:title="data.title" />
+		<article class="article__card" v-if="responseData" v-for="(data, index) in responseData" :key="index">
+			<a href="data.url">
+				<picture class="card__image">
+					<img :src="data.image" :alt="data.description">
+				</picture>
+			</a>
+			<p class="card__author" v-if="data.author">
+				<small><span v-if="data.authorLabel">{{ data.authorLabel }}</span>{{ data.author }}</small>
+			</p>
+			<h3 class="card__title" v-if="data.title"><a :href="data.url">{{ data.title }}</a></h3>
+			<p class="card__description" v-if="data.description">{{ data.description }}</p>
+		</article>
 	</section>
 </template>
 
 <script>
-import ArticleCard from '@/components/ArticleCard/ArticleCard';
 
 export default {
-	components: {
-    ArticleCard
-  },
 	props: {
 		responseData: {
       type: Array,
