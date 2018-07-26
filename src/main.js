fdc3384e696685487f7bb23986cf64a54967ab1b
Jason Witt <jawittdesigns@gmail.com>
2018-07-26 06:21:21 -0400

    Updated import paths

--- src/main.js
+++ src/main.js
@@ -1,42 +1,15 @@
 import Vue from 'vue';
 import App from '@/App.vue';
-import router from '@/router';
-import store from '@/store/store';
+import router from '@/router/index';
+import store from '@/store/index';
 import '@/registerServiceWorker';
 
-import { mapState, mapGetters, mapMutations } from 'vuex';
+// Import mixins
+import { excludeMenuMixin } from './mixins/excludeMenuMixin';
 
 new Vue({
   router,
 	store,
+	mixins: [excludeMenuMixin],
 	render: h => h(App),
-	methods: {
-		...mapMutations('excludeNav', [
-			'excludeNavTrue',
-			'excludeNavFalse'
-		]),
-		excludeNavBar() {
-			this.excludeNavFalse()
-			this.getRouteData.forEach(item => {
-				if( item.path === this.$route.path && item.exclude ) {
-					this.excludeNavTrue()
-				}
-			})
-		}
-	},
-	computed: {
-		...mapState('excludeNav', [
-			'excludeNav'
-		]),
-		...mapGetters('routes', [
-			'getRouteData',
-		])
-	},
-	watch: {
-		// see: https://www.vuemastery.com/conferences/vueconf-2018/7-secret-patterns-vue-consultants-dont-want-you-to-know-chris-fritz/
-		$route: {
-			handler: 'excludeNavBar',
-			immediate: true
-		}
-	}
 }).$mount('#app');
