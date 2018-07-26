404d754ba16c5edcb484620d587efa25c2af8ce5
Jason Witt <jawittdesigns@gmail.com>
2018-07-26 06:22:33 -0400

    Renamed base store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-// Import Modules
-// import produce from './modules/produce';
-// import routes from './modules/routes';
-// import excludeNav from './modules/excludeNav';
 import modules from './modules'
 
 Vue.use(Vuex)
