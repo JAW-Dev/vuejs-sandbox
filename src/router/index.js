b5ceee5833f3e68f10d66257fdaa6de1bb033a3a
Jason Witt <jawittdesigns@gmail.com>
2018-07-26 06:22:09 -0400

    Put base router file in it’s own dir

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import Vue from 'vue';
+import Router from 'vue-router';
+// Import Configs
+import { baseURL } from '@/config/app';
+
+// Import routes
+import AppRoutes from '@/router/routes/AppRoutes';
+
+Vue.use(Router);
+
+// Wildcard redirect to base URL for any undefined URLs.
+const wildcardRedirect = [ { path: '*', redirect: '/' } ];
+
+export default new Router({
+	base: baseURL,
+	// Concatenate all the routes.
+  routes: [...wildcardRedirect, ...AppRoutes]
+});
