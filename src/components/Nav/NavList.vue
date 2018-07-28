480732440ba2a5b0b54d8693174807794712613b
Jason Witt <jawittdesigns@gmail.com>
2018-07-28 18:01:29 -0400

    Added NavList component

---
+++ src/components/Nav/NavList.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="nav" id="nav" v-if="routes">
+		<slot></slot>
+    <ul class="nav__list">
+      <li class="list__item" v-for="(route, index) in routes" :key="index">
+        <router-link 
+					:to="route.path"
+					:key="$route.fullPath">{{ route.title }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+	props: {
+		routes: {
+      type: Array,
+      required: true,
+      default: () => []
+		},
+	},
+};
+</script>
