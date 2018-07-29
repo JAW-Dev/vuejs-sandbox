d968f8651616da22ba62512846d7652ae09a41e9
Jason Witt <jawittdesigns@gmail.com>
2018-07-29 09:30:57 -0400

    Added alerts to routes

--- src/router/routes/AppRoutes.js
+++ src/router/routes/AppRoutes.js
@@ -3,6 +3,7 @@ import Home from '@/views/HomeView';
 import AutocompleteField from '@/views/AutocompleteFieldView';
 import ArticleAggregator from '@/views/ArticleAggregatorView';
 import Tabs from '@/views/TabsView';
+import Alerts from '@/views/AlertsView';
 
 // Define the routes.
 // exclude: when on this route remove the main navigation.
@@ -10,5 +11,6 @@ export default [
 	{ path: '/', name: 'home', component: Home, title: 'Home' },
 	{ path: '/autocomplete-field', name: 'autocomplete-field', component: AutocompleteField, title: 'Autocomplete Field' },
 	{ path: '/article-aggregator', name: 'article-aggregator', component: ArticleAggregator, title: 'Article Aggregator' },
-	{ path: '/tabs', name: 'tabs', component: Tabs, title: 'Tabs' }
+	{ path: '/tabs', name: 'tabs', component: Tabs, title: 'Tabs' },
+	{ path: '/alerts', name: 'alerts', component: Alerts, title: 'Alerts' }
 ];
