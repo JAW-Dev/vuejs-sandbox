b67d85511c1360fdfb5a2394bee96b019811f978
Jason Witt <jawittdesigns@gmail.com>
2018-07-31 08:48:13 -0400

    Added Alert component

---
+++ src/components/Alerts/Alert.vue
@@ -0,0 +1,27 @@
+<template>
+	<div v-if="properties" class="alerts" >
+		<Flyout 
+			v-if="properties.type === 'flyout'"
+			:property="properties"
+			:classes="'alert__'+ properties.type ">
+			<slot name="alert-icon" slot="icon"></slot>
+		</Flyout>
+	</div>
+</template>
+
+<script>
+import Flyout from './Flyout'
+
+export default {
+	components: {
+		Flyout
+	},
+	props: {
+		properties: {
+			type: Object,
+			required: true,
+			default: () => {}
+		}
+	}
+}
+</script>
