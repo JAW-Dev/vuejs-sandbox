0803bab24cee365039bce796295dde634ca59196
Jason Witt <jawittdesigns@gmail.com>
2018-08-01 14:51:34 -0400

    Moved stles to view

--- src/components/AutocompleteField/AutocompleteField.vue
+++ src/components/AutocompleteField/AutocompleteField.vue
@@ -93,37 +93,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.autocomplete {
-  position: relative;
-  display: block;
-  margin: auto;
-  width: px-to-rem(250);
-}
-.autocomplete__input {
-  background: $white;
-  border: 1px solid darken($grey90, 10%);
-  padding: 0.2rem 0.5rem;
-  width: 100%;
-  &:hover {
-    background: darken($grey90, 10%);
-  }
-}
-.autocomplete__results {
-  position: absolute;
-  background-color: $white;
-  border: 1px solid darken($grey90, 10%);
-  width: 100%;
-  z-index: 1;
-}
-.results__item {
-  color: $black;
-  cursor: pointer;
-  display: block;
-  padding: 0.2rem 0.5rem;
-  &:hover {
-    background-color: darken($grey90, 10%);
-  }
-}
-</style>
