92b6ef9537fd3d55258d41457a0e22e5c099dc63
Jason Witt <jawittdesigns@gmail.com>
2018-08-02 15:40:04 -0400

    Added options to Image component

--- src/components/Articles/_partials/Image.vue
+++ src/components/Articles/_partials/Image.vue
@@ -1,19 +1,54 @@
 <template>
-  <a class="card__image-link" v-if="article.url || article.image" :href="article.url">
-    <picture class="card__image-wrapper" v-if="article.image">
-      <img class="card__image" v-if="article.image" :src="article.image" :alt="article.description">
+  <card-link
+    :id="options.image.imageLink.ID"
+    :class="options.image.imageLink.classes"
+    v-bind="options.articles.attributes"
+    :href="article.url"
+    :renderLink="renderLink"
+    :renderElement="renderElement">
+    <picture class="card__image-wrapper" slot="content" v-if="renderImage">
+      <img class="card__image" :src="article.image" :alt="article.description" v-if="renderImage">
     </picture>
-  </a>
+  </card-link>
 </template>
 
 <script>
+import Link from './Link'
+
 export default {
   name: 'card-image',
-  components: {},
+  components: {
+    'card-link': Link
+  },
   props: {
     article: {
       type: Object,
       default: () => {}
+    },
+    options: {
+      type: Object,
+      required: false,
+      default: () => {}
+    }
+  },
+  computed: {
+    renderLink() {
+      if (this.article.image && this.options.image.linkImage) {
+        return true
+      }
+      return false
+    },
+    renderElement() {
+      if (this.article.image && !this.options.image.linkImage && this.options.image.showImage) {
+        return true
+      }
+      return false
+    },
+    renderImage() {
+      if (this.article.image && this.options.image.showImage) {
+        return true
+      }
+      return false
     }
   }
 }
