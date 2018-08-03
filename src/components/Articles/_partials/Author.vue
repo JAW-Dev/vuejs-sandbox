cc2b8677261f1ac07a979c85cfd6b94e42ccc767
Jason Witt <jawittdesigns@gmail.com>
2018-08-03 16:10:11 -0400

    Refactored for dynamic data

--- src/components/Articles/_partials/Author.vue
+++ src/components/Articles/_partials/Author.vue
@@ -3,7 +3,7 @@
     :id="options.author.ID"
     :class="options.author.classes"
     v-bind="options.author.attributes"
-    v-if="article.author && options.author.showAuthor">
+    v-if="options.author.showAuthor && article[options.author.source]">
     <span
       :id="options.author.label.ID"
       :class="options.author.label.classes"
@@ -13,22 +13,22 @@
       :theId="options.author.link.ID"
       :classes="options.author.link.classes"
       :attributes="options.author.link.attributes"
-      :theHref="article.authorUrl"
-      :renderLink="renderElement(article.authorUrl && options.author.link.showLink)">
+      :theHref="article[options.author.link.source]"
+      :renderLink="renderElement(article[options.author.link.source] && options.author.link.showLink)">
       <span
         slot="content"
         :id="options.author.text.ID"
         :class="options.author.text.classes"
-        v-bind="options.author.text.attributes">{{ article.author }}</span>
+        v-bind="options.author.text.attributes">{{ article[options.author.source] }}</span>
     </card-link>
   </p>
 </template>
 
 <script>
-import { renderElement, props, link } from '@/mixins/cardElements'
+import { components, props, methods } from '@/mixins/cardElements'
 
 export default {
   name: 'card-author',
-  mixins: [renderElement, props, link]
+  mixins: [components, props, methods]
 }
 </script>
--- src/components/Articles/_partials/Image.vue
+++ src/components/Articles/_partials/Image.vue
@@ -3,29 +3,29 @@
     :theId="options.image.link.ID"
     :classes="options.image.link.classes"
     :attributes="options.image.link.attributes"
-    :theHref="article.url"
-    :renderLink="renderElement(article.image && options.image.link.showLink)">
+    :theHref="article[options.image.link.source]"
+    :renderLink="renderElement(options.image.link.showLink)">
     <picture
       :id="options.image.wrapper.ID"
       :class="options.image.wrapper.classes"
       v-bind="options.image.wrapper.attributes"
-      v-if="this.article.image && this.options.image.showImage"
+      v-if="article[options.image.source] && options.image.showImage"
       slot="content">
       <img
         :id="options.image.ID"
         :class="options.image.classes"
         v-bind="options.image.attributes"
-        :src="article.image"
+        :src="article[options.image.source]"
         :alt="article.description">
     </picture>
   </card-link>
 </template>
 
 <script>
-import { renderElement, props, link } from '@/mixins/cardElements'
+import { components, props, methods } from '@/mixins/cardElements'
 
 export default {
   name: 'card-image',
-  mixins: [renderElement, props, link]
+  mixins: [components, props, methods]
 }
 </script>
