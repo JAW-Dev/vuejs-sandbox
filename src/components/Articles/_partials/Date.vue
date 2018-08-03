5856c684d111581fe3dbae110177dfa75dcbb39c
Jason Witt <jawittdesigns@gmail.com>
2018-08-03 16:09:48 -0400

    Refactored data component

--- src/components/Articles/_partials/Date.vue
+++ src/components/Articles/_partials/Date.vue
@@ -1,18 +1,44 @@
 <template>
-  <p class="card__date" v-if="article.date">
-    <span class="card__date-label" v-if="article.dateLabel">{{ article.dateLabel }}</span>{{ article.date }}
+  <p
+    :id="options.date.ID"
+    :class="options.date.classes"
+    v-bind="options.date.attributes"
+    v-if="options.date.showDate && article[options.date.source]">
+    <span
+      :id="options.date.label.ID"
+      :class="options.date.label.classes"
+      v-bind="options.date.label.attributes"
+      v-if="options.date.label.showLabel">{{ options.date.label.labelText }}</span>
+    <card-link
+      :theId="options.date.link.ID"
+      :classes="options.date.link.classes"
+      :attributes="options.date.link.attributes"
+      :theHref="article[options.date.link.source]"
+      :renderLink="renderElement(article[options.date.link.source] && options.date.link.showLink)">
+        <span
+          slot="content"
+          :id="options.date.text.ID"
+          :class="options.date.text.classes"
+          v-bind="options.date.text.attributes">
+            {{
+              article[options.date.source] |
+              dateFormat(
+                options.date.format.doFormat,
+                options.date.format.locale,
+                options.date.format.options
+              )
+            }}
+            <!-- {{ article[options.date.source] }} -->
+          </span>
+    </card-link>
   </p>
 </template>
 
 <script>
+import { components, props, methods, filters } from '@/mixins/cardElements'
+
 export default {
   name: 'card-date',
-  components: {},
-  props: {
-    article: {
-      type: Object,
-      default: () => {}
-    }
-  }
+  mixins: [components, props, methods, filters]
 }
 </script>
