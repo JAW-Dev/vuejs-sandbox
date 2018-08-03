3f71af423d8f41b077b46280caa44bf5a51d047a
Jason Witt <jawittdesigns@gmail.com>
2018-08-03 17:43:26 -0400

    Refactored Title component

--- src/components/Articles/_partials/Title.vue
+++ src/components/Articles/_partials/Title.vue
@@ -1,18 +1,29 @@
 <template>
-  <h3 class="card__title" v-if="article.title">
-    <a class="card__title-link" v-if="article.url" :href="article.url">{{ article.title }}</a>
+  <h3
+    :id="options.title.ID"
+    :class="options.title.classes"
+    v-bind="options.title.attributes"
+    v-if="options.title.show && article[options.title.source]">
+    <card-link
+      :theId="options.title.link.ID"
+      :classes="options.title.link.classes"
+      :attributes="options.title.link.attributes"
+      :theHref="article[options.title.link.source]"
+      :renderLink="renderElement(article[options.title.link.source] && options.title.link.show)">
+      <span
+        slot="content"
+        :id="options.title.text.ID"
+        :class="options.title.text.classes"
+        v-bind="options.title.text.attributes">{{ article[options.title.source] }}</span>
+    </card-link>
   </h3>
 </template>
 
 <script>
+import { components, props, methods } from '@/mixins/cardElements'
+
 export default {
-  name: 'card-title',
-  components: {},
-  props: {
-    article: {
-      type: Object,
-      default: () => {}
-    }
-  }
+  name: 'card-author',
+  mixins: [components, props, methods]
 }
 </script>
