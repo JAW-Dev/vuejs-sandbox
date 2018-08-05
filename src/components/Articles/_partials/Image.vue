7a1864edc5e592870cf7fc2f7b91184193ccfc43
Jason Witt <jawittdesigns@gmail.com>
2018-08-04 23:08:51 -0400

    Added image caption and replaced picture with figure tag

--- src/components/Articles/_partials/Image.vue
+++ src/components/Articles/_partials/Image.vue
@@ -5,19 +5,27 @@
     :attributes="options.image.link.attributes"
     :theHref="getUrl(options.sources.imageUrl)"
     :renderLink="renderElement(getUrl(options.sources.imageUrl) && isSet(options.image.link.show))">
-    <picture
+    <figure
       :id="options.image.wrapper.ID"
       :class="options.image.wrapper.classes"
       v-bind="options.image.wrapper.attributes"
-      v-if="getSource(article[options.sources.image]) && isSet(options.image.show)"
+      v-if="article[options.sources.image] && options.image.show"
       slot="content">
       <img
         :id="options.image.ID"
         :class="options.image.classes"
         v-bind="options.image.attributes"
-        :src="getSource(article[options.sources.image])"
-        :alt="article.description">
-    </picture>
+        :src="article[options.sources.image]"
+        :alt="article.description"
+        v-if="article[options.sources.image]">
+        <figcaption
+          :id="options.image.caption.ID"
+          :class="options.image.caption.classes"
+          v-bind="options.image.caption.attributes"
+          v-if="options.image.caption.show">
+          {{ isSet(article[options.sources.imageCaption]) }}
+        </figcaption>
+    </figure>
   </card-link>
 </template>
 
